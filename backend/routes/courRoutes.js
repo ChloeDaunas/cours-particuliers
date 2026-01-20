2cd9bcece3c5ebb9847b4ee141c20dd301cbfd0f
ChloeDaunas <andr3amila@gmail.com>
2026-01-20 15:07:16 +0100

    toutes les routes

--- backend/routes/courRoutes.js
+++ backend/routes/courRoutes.js
@@ -12,4 +12,40 @@ router.post('/add', async (req, res) => {
     }   
 });
 
+router.get('/all', async (req, res) => {
+    try {
+        const listeCours = await cour.find().populate('eleveId');
+        res.status(200).json(listeCours);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }   
+});
+
+router.patch('/update/:id', async (req, res) => {
+    try {
+        // On cherche par l'ID et on applique les changements contenus dans req.body
+        const updatedCour = await cour.findByIdAndUpdate(
+            req.params.id, 
+            req.body, 
+            { new: true } // Cette option permet de renvoyer l'objet APRÈS modification
+        );
+        res.status(200).json(updatedCour);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        await cour.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: 'Cours supprimé avec succès' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+
+
+
 export default router;
--- backend/routes/eleveRoutes.js
+++ backend/routes/eleveRoutes.js
@@ -18,4 +18,38 @@ router.post('/add', async (req, res) => {
     }
 });
 
+router.get('/all', async (req, res) => {
+    try {
+        const listeEleves = await eleve.find();
+
+         res.status(200).json(listeEleves);
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.patch('/update/:id', async (req, res) => {
+    try {
+        // On cherche par l'ID et on applique les changements contenus dans req.body
+        const updatedEleve = await eleve.findByIdAndUpdate(
+            req.params.id, 
+            req.body,
+            { new: true } // Cette option permet de renvoyer l'objet APRÈS modification
+        );
+        res.status(200).json(updatedEleve);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        await eleve.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: 'Élève supprimé avec succès' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 export default router;
